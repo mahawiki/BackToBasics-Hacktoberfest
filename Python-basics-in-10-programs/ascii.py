6039f86d15ec7dc3cae798129ccaadb5ca84a80c
Aadesh Jadhav <55881613+adijdhv@users.noreply.github.com>
2020-10-04 22:36:46 +0530

    ASCII VALUE
    It is program for beginner to know the ASCII VALUE FOR ANY CHARACTER

---
+++ Python-basics-in-10-programs/ascii.py
@@ -0,0 +1,4 @@
+#here is the short program for finding the ASCII VALUE using python 
+
+character = input('Enter the key to know ASCII VALUE ')
+print("The ASCII value of '" + character + "' is", ord(character))
