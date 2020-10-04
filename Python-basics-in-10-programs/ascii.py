a415683ae83d6d9228a9d17d0a4ce41cc12df5f3
Aadesh Jadhav <55881613+adijdhv@users.noreply.github.com>
2020-10-04 22:53:23 +0530

    Delete ascii.py

--- Python-basics-in-10-programs/ascii.py
+++
@@ -1,4 +0,0 @@
-#here is the short program for finding the ASCII VALUE using python 
-
-character = input('Enter the key to know ASCII VALUE ')
-print("The ASCII value of '" + character + "' is", ord(character))
