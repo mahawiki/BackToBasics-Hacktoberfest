7bc752a52763f2737090f4dbc0b1ec05309277b3
Aadesh Jadhav <55881613+adijdhv@users.noreply.github.com>
2020-10-05 01:45:09 +0530

    Update ascii.py

--- Python-basics-in-10-programs/ascii.py
+++ Python-basics-in-10-programs/ascii.py
@@ -1,5 +1,5 @@
 
 # Program to find the ASCII value of the given character
 
-cha = input('Enter The Character ')
+cha = input('Enter The Character - ')
 print("The ASCII value of '" + cha + "' is", ord(cha))
